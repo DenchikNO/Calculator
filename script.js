6bbb08b9802b790d49b37f4189e1f60636757a52
DenchikNO <44582128+DenchikNO@users.noreply.github.com>
2020-01-18 20:02:46 +0300

    2

---
+++ script.js
@@ -0,0 +1,9 @@
+function set(num) {
+  document.form.textview.value = document.form.textview.value + num;
+}
+function set_simvol(simvol) {
+  document.form.textview.value = simvol;
+}
+function delete_all() {
+  document.form.textview.value = "";
+}
