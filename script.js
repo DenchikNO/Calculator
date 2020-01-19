2f8520c54687943299a184cd6c1d7a15de81270a
DenchikNO <44582128+DenchikNO@users.noreply.github.com>
2020-01-19 20:14:57 +0300

    version 3

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function set(num) {
-  document.form.textview.value = document.form.textview.value + num;
+function set(n) {
+  document.form.textview.value = document.form.textview.value + n;
 }
-function set_simvol(simvol) {
-  document.form.textview.value = simvol;
+function equal() {
+  let x = document.form.textview.value;
+  if (x) {
+    document.form.textview.value = eval(x);
+  }
 }
 function delete_all() {
   document.form.textview.value = "";
+}
+function delete_n() {
+  let x = document.form.textview.value;
+  document.form.textview.value = x.substring(0, x.length - 1);
+}
+function reverse() {
+  document.form.textview.value = -document.form.textview.value;
 }
